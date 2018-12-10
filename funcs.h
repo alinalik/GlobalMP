12f59e69ff647a7f91e5a6a8ba7d85fc622efa3e
AlinaR <alina-r08@yandex.ru>
2018-12-10 16:30:56 +0300

    add function MyPow()

--- funcs.h
+++ funcs.h
@@ -3,8 +3,7 @@
 #define __FUNCS_H__
 double MySum(double, double);
 double MySub(double, double);
-double MyMul(double, double);double MyDiv(double a, double b)
-{
-return a / b;
-}
+double MyMul(double, double);
+double MyDiv(double a, double b)
+int MyPow(int, int);
 #endif // __FUNCS_H__
